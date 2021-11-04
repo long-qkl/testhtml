d5808cc97f73369feffc0c2feb006d7a4f0640df
qiukailong <1579714434@qq.com>
2021-11-04 18:00:18 +0800

    创建react项目todolist

---
+++ two_por/src/Todo.js
@@ -0,0 +1,14 @@
+import './Todo.css';
+
+function Todo() {
+    return(
+        <div className="bbg">
+            <img src="https://www.shijuepi.com/uploads/allimg/200918/1-20091Q10420.jpg" alt="图片接受失败"></img>
+            <div className="sub">
+                
+            </div>
+        </div>
+    );
+}
+
+export default Todo;
