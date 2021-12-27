1d170c52a4f04fe4f6f54eccac6ffba98c1f6ff0
qiukailong <1579714434@qq.com>
2021-12-27 18:01:37 +0800

    111

--- three_por/src/App.js
+++ three_por/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import './CSS/App.min.css';
+import Child from './Child';
+import axios from 'axios';
+import { useEffect } from 'react';
 
 function App() {
+
+  useEffect(()=>{
+    axios.get("/ajax/movieOnInfoList?token=&optimus_uuid=CA784160093811EC8C7CF53920300F044C4DB2FE7FBE4254B4C91F97D6FC2495&optimus_risk_level=71&optimus_code=10").then(res=>{
+      console.log(res.data)
+    })
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        <li>app</li>
+        <li>app</li>
+        <li>app</li>
+        <li>app</li>
+      </ul>
+      <Child></Child>
     </div>
   );
 }
---
+++ three_por/src/Child.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './CSS/Child.min.css';
+
+function Child(props) {
+    return (
+        <div className='child'>
+            <ul>
+                <li>Child</li>
+                <li>Child</li>
+                <li>Child</li>
+                <li>Child</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Child;
