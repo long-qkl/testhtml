4e4ace9cfd8d970be1662afe4d5c120ad162bc4c
long-qkl <1579714434@qq.com>
2022-01-09 15:31:54 +0800

    topheader右侧完成

--- three_por/src/components/sandbox/SideMenu.js
+++ three_por/src/components/sandbox/SideMenu.js
@@ -15,7 +15,7 @@ export default function SideMenu() {
           <div className="logo" />
           <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
             <Menu.Item key="1" icon={<UserOutlined />}>
-              nav 1
+              nav 1 
             </Menu.Item>
             <Menu.Item key="2" icon={<VideoCameraOutlined />}>
               nav 2
--- three_por/src/components/sandbox/TopHeader.js
+++ three_por/src/components/sandbox/TopHeader.js
@@ -1,19 +1,45 @@
 import React,{useState} from 'react'
-import { Layout } from 'antd'
+import { Layout, Menu, Dropdown, message,Avatar } from 'antd'
 import {
     MenuFoldOutlined,
-    MenuUnfoldOutlined
+    MenuUnfoldOutlined,
+    // DownOutlined,
+    UserOutlined,
 } from '@ant-design/icons'
 
 const { Header } = Layout;
 
 export default function TopHeader() {
-    const [collapsed, setCollapsed] = useState(false)
+    const [collapsed,setCollapsed] = useState(false)
+    const changeColeapsed = ()=>{
+        setCollapsed(!collapsed)
+    }
+    const onClick=({key})=>{
+        message.info(`Click on item ${key}`)
+    }
+    const menu = (
+        <Menu onClick={onClick}>
+          <Menu.Item key="0">
+              超级管理员
+          </Menu.Item>
+          <Menu.Divider />
+          <Menu.Item key="1" danger>
+            退出
+          </Menu.Item>
+        </Menu>
+      );
+
     return (
-        <Header className='site-layout-background' style={{ padding:0 }}>
+        <Header className='site-layout-background' style={{ padding: '0 16px' }}>
             {
-                collapsed ? <MenuUnfoldOutlined /> :<MenuFoldOutlined />
+                collapsed ? <MenuUnfoldOutlined onClick={changeColeapsed} /> : <MenuFoldOutlined onClick={changeColeapsed} />
             }
+            <div style={{ float : 'right' }} >
+            <span style={{ marginRight : '5px' }} >欢迎回来admin</span>
+            <Dropdown overlay={menu} trigger={['click']}>
+                <Avatar size="large" icon={<UserOutlined />} />
+            </Dropdown>
+            </div>
         </Header>
     )
 }
