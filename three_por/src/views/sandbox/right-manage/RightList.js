ac3cb4cca2dde2bd526a0c3872f8c930f916626d
qiukailong <1579714434@qq.com>
2022-02-09 09:28:51 +0800

    111

--- three_por/src/views/sandbox/right-manage/RightList.js
+++ three_por/src/views/sandbox/right-manage/RightList.js
@@ -1,6 +1,19 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
 
 export default function RightList() {
+    const locationUrl=useLocation();
+    let setUrl=locationUrl.pathname
+    console.log(setUrl);
+    useEffect(()=>{
+        // setInterval(()=>{
+            axios.get("http://localhost:8000/rights").then(res=>{
+                console.log(res.data);
+            })
+        // },1000)
+        console.log('RightList进来了');
+    },[])
     return (
         <div>
             RightList111
