d9af40a293dcf4979e3e6937480a34c7820c6619
qiukailong <1579714434@qq.com>
2022-04-19 17:26:43 +0800

    登录页面样式待修改,即将添加粒子效果

--- three_por/src/components/sandbox/TopHeader.js
+++ three_por/src/components/sandbox/TopHeader.js
@@ -1,5 +1,7 @@
-import React,{useState} from 'react'
-import { Layout, Menu, Dropdown, message,Avatar } from 'antd'
+import React, { useState } from 'react'
+import { Layout, Menu, Dropdown, message, Avatar } from 'antd'
+// import {withRouter} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
 import {
     MenuFoldOutlined,
     MenuUnfoldOutlined,
@@ -10,35 +12,40 @@ import {
 const { Header } = Layout;
 
 export default function TopHeader() {
-    const [collapsed,setCollapsed] = useState(false)
-    const changeColeapsed = ()=>{
+    const [collapsed, setCollapsed] = useState(false)
+
+    const navigate = useNavigate()
+    const changeColeapsed = () => {
         setCollapsed(!collapsed)
     }
-    const onClick=({key})=>{
+    const onClick = ({ key }) => {
         message.info(`Click on item ${key}`)
     }
     const menu = (
         <Menu onClick={onClick}>
-          <Menu.Item key="0">
-              超级管理员
-          </Menu.Item>
-          <Menu.Divider />
-          <Menu.Item key="1" danger>
-            退出
-          </Menu.Item>
+            <Menu.Item key="0">
+                超级管理员
+            </Menu.Item>
+            <Menu.Divider />
+            <Menu.Item key="1" danger onClick={() => {
+                localStorage.removeItem('token')
+                navigate("/login")
+            }}>
+                退出
+            </Menu.Item>
         </Menu>
-      );
+    );
 
     return (
         <Header className='site-layout-background' style={{ padding: '0 16px' }}>
             {
                 collapsed ? <MenuUnfoldOutlined onClick={changeColeapsed} /> : <MenuFoldOutlined onClick={changeColeapsed} />
             }
-            <div style={{ float : 'right' }} >
-            <span style={{ marginRight : '5px' }} >欢迎回来admin</span>
-            <Dropdown overlay={menu} trigger={['click']}>
-                <Avatar size="large" icon={<UserOutlined />} />
-            </Dropdown>
+            <div style={{ float: 'right' }} >
+                <span style={{ marginRight: '5px' }} >欢迎回来admin</span>
+                <Dropdown overlay={menu} trigger={['click']}>
+                    <Avatar size="large" icon={<UserOutlined />} />
+                </Dropdown>
             </div>
         </Header>
     )
