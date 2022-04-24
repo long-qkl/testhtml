bc89ee43051ac3c16f3f3fc107b0eb0825eda0d2
qiukailong <1579714434@qq.com>
2022-04-24 13:44:10 +0800

    todolist界面增加粒子特效

--- two_por/src/Todo.js
+++ two_por/src/Todo.js
@@ -1,17 +1,18 @@
 // import { Component } from 'react';
 import React from 'react';
 import './CSS/Todo.min.css';
-import bgimg from './images/122.jpg';
+// import bgimg from './images/122.jpg';
 import Input from './Input';
 import Lists from './Lists';
+import ReactCanvasNest from 'react-canvas-nest'
 
 class Todo extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props)
-        this.state={
-            lists:['12','32','fr'],
-            finishList:[12,3],
-            inputVal:'',
+        this.state = {
+            lists: ['12', '32', 'fr'],
+            finishList: [12, 3],
+            inputVal: '',
 
         }
         // this.onput=this.onput.bind(this)
@@ -26,22 +27,22 @@ class Todo extends React.Component {
     //         inputVal: e.target.value
     //     })
     // }
-    onput=(e)=>{
+    onput = (e) => {
         this.setState({
             inputVal: e.target.value
         })
     }
 
     // 组件Input中的button框调用的方法
-    onsub=(e)=>{
+    onsub = (e) => {
         //添加输入的数据到开头
         // lists.unshift()
-        if(this.state.inputVal){
+        if (this.state.inputVal) {
             this.setState({
-                lists: [this.state.inputVal,...this.state.lists],
-                inputVal:''
+                lists: [this.state.inputVal, ...this.state.lists],
+                inputVal: ''
             })
-        }else{
+        } else {
             console.log("输入为空哦！")
         }
         e.preventDefault();//阻止默认事件
@@ -50,42 +51,54 @@ class Todo extends React.Component {
     //点击列表中的待办事件
     //得搞定父子组件传参，函数式父子组件传参
     //父传子
-    mvcl=(index)=>{
+    mvcl = (index) => {
         // console.log(e.target.attributes.idx.value,"mvcl")
         // let idx=e.target.attributes.idx.value
         console.log(index)
-        let data=this.state.lists.splice(index,1)
+        let data = this.state.lists.splice(index, 1)
 
         this.setState({
-            lists: this.state.lists.filter((item)=>{
+            lists: this.state.lists.filter((item) => {
                 return item !== data
             }),
-            finishList: [data,...this.state.finishList]
-        })  
+            finishList: [data, ...this.state.finishList]
+        })
     }
-    rmcl=(index)=>{
+    rmcl = (index) => {
         // console.log("点击了列表中的删除事件，彻底删除掉")
         // console.log(index,"rmcl")
         // let idx=e.target.attributes.idx.value
-        let data=this.state.finishList.splice(index,1)
+        let data = this.state.finishList.splice(index, 1)
         this.setState({
-            finishList: this.state.finishList.filter((item)=>{
+            finishList: this.state.finishList.filter((item) => {
                 return item !== data
             })
         })
     }
 
     //input回车事件
-    inputKeyup=(e)=>{
-        if(e.keyCode===13){
+    inputKeyup = (e) => {
+        if (e.keyCode === 13) {
             this.onsub(e)
         }
     }
-    
-    render(){
-        return(
+
+    render() {
+        return (
             <div className="bbg">
                 {/* <img src={bgimg} alt="图片接收失败"></img> */}
+                
+                <ReactCanvasNest
+                    className='canvasNest'
+                    config={{
+                        pointColor: ' 255, 255, 255 ',
+                        lineColor: '255,255,255',
+                        pointOpacity: 0.5,
+                        pointR: 2,
+                        count:100
+                    }}
+                    style={{ zIndex: 1 }}
+                />
                 <div className="sub">
                     <Input onsub={this.onsub} onput={this.onput} inputVal={this.state.inputVal} inputKeyup={this.inputKeyup} />
                     <span>任务列表</span>
