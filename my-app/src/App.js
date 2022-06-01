1af789283e51cbeee86d980217bb298e26b1d352
qiukailong <1579714434@qq.com>
2022-06-01 18:02:38 +0800

    1111

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import ReactCanvasNest from 'react-canvas-nest'
+
+function App() {
+  return (
+    <div className="App">
+      {/* <div>222</div> */}
+
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/bg.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./bg.css"
+
+export default function bg() {
+    return (
+        <div className='bg'>
+            <img src='https://long-qkl.github.io/testhtml/two_por/src/images/122.jpg' alt='无图片存在' style={{ height: 800 }}></img>
+            <div>sdfsf</div>
+        </div>
+    )
+}
