627f829bd82dfc8b789339f47af35c4278ea0f28
qiukailong <1579714434@qq.com>
2022-12-28 15:42:30 +0800

    添加配置文件

---
+++ node_test/node_por/config.js
@@ -0,0 +1,15 @@
+const config={
+    // 端口号
+    APP_PORT: 3333,
+    mongodb: "require('mongodb').MongoClient",
+    url: "mongodb://localhost:27017",
+    db_name: "yzzy"
+}
+
+module.exports=config
+
+
+//mongodb
+const mongodb = require('mongodb').MongoClient;
+const url="mongodb://localhost:27017"
+const db_name="yzzy";
